1db8b92fc00dfa882af7613eb81312cdeef74167
Anosh Malik <anoshmalik.am@gmail.com>
2022-11-22 22:44:55 +0000

    A - Car sales - unfinished

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,13 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+    let totalVal = {};
+    // for (let object of carsSold) {
+    //     // let x = object.filter(value => value.price);
+    //     // totalVal += `${object.make} : ${object.price} `; 
+    //     console.log(Object.keys(object));
+    
+    // }
+    console.log(carsSold.filter(value => value.make == "Land Rover"));    
+
+}
 
 module.exports = sales;
